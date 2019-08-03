05edb29f83cceeb6049e420565bde04395366754
Jester <jester@MacBook-Pro-de-Jester.local>
2019-08-03 21:30:50 +0100

    prototype navigation

--- main.c
+++ main.c
@@ -1,4 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "SDL2/SDL.h"
+
+int nav(int x_position, int y_position, int x_limit, int y_limit)
+{
+
+    for (int i = x_position; i <= x_limit; ++i)
+    {
+        return(i);
+    }
+    return x_position;
+    
+}
 
 int main(int argc, char* argv[])
 {
@@ -7,12 +21,14 @@ int main(int argc, char* argv[])
         SDL_Window* window = NULL;
         SDL_Renderer* renderer = NULL;
 
-        if (SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer) == 0) 
+        int resolutionL = 640;
+        int resolutionH = 480;
+
+        if (SDL_CreateWindowAndRenderer(resolutionL, resolutionH, 0, &window, &renderer) == 0) 
         {
             SDL_bool done = SDL_FALSE;
 
-            int x = 300;
-            int y = 240;
+            int x = 300, y = 200, i = 0, j = 0;
 
             while (!done) 
             {
@@ -22,19 +38,21 @@ int main(int argc, char* argv[])
                 SDL_RenderClear(renderer);
 
                 SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+
+                SDL_RenderDrawLine(renderer, 10, 10, 10, (resolutionH - 10));
+                SDL_RenderDrawLine(renderer, 10, 10, (resolutionL - 10), 10);
+                SDL_RenderDrawLine(renderer, (resolutionL - 10), 10, (resolutionL - 10), (resolutionH - 10));
+                SDL_RenderDrawLine(renderer, 10, (resolutionH - 10), (resolutionL - 10), (resolutionH - 10));
+
                 SDL_RenderDrawPoint(renderer, x, y);
                 SDL_RenderPresent(renderer);
+
+
                 
-                SDL_Delay(100);
-                if(y < x)
-                {
-                y++;
-                }
-                else
-                {
-                    x++;
-                }
-                
+                x = nav(x, y, (resolutionL - 20), (resolutionH - 20));
+                //y = nav(x, y, (resolutionL - 20), (resolutionH - 20));
+
+                SDL_Delay(20);
 
                 while (SDL_PollEvent(&event)) 
                 {
